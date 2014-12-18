a3fbfcdbd9dcf34c23c6caa717a8491c657eb265
Ronald Kasagga <ronald.kasagga.k3@gmail.com>
2014-12-18 12:14:14 +0300

    updated db configuration

--- protected/config/database.php
+++ protected/config/database.php
@@ -5,10 +5,10 @@ return array(
 	//'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 	// uncomment the following lines to use a MySQL database
 
-	'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+	'connectionString' => 'mysql:host=127.12.128.2;dbname=katale',
 	'emulatePrepare' => true,
-	'username' => 'root',
-	'password' => '',
+	'username' => 'adminQcnWdUk',
+	'password' => '6etHKBDM7xH6',
 	'charset' => 'utf8',
 
 );
