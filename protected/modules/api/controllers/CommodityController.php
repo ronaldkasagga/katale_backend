75a2c760fdd787fca39a62f22ea92ad1b8858302
Ronald Kasagga <ronald.kasagga.k3@gmail.com>
2014-12-18 12:06:38 +0300

    added commodity add and update functionality

---
+++ protected/modules/api/controllers/CommodityController.php
@@ -0,0 +1,51 @@
+<?php
+
+class CommodityController extends Controller
+{
+	public function add()
+	{
+        $response = array('success'=>'ok');
+        $commodity_d = Yii::app()->request->getParam('commodityId');
+        $price = Yii::app()->request->getParam('price');
+        $quantity = Yii::app()->request->getParam('quantity');
+        $vendor_id = Yii::app()->request->getParam('vendorId');
+        $commodity = new VendorComodity();
+        $commodity->price = $price;
+        $commodity->comodity_id = $commodity_d;
+        $commodity->quantity = $quantity;
+        $commodity->vendor_id = $vendor_id;
+        if(!$commodity->save()){
+            $response['success'] = 'failed';
+            $response['detail'] = $commodity->getErrors();
+        }
+        header('Content-type: application/json');
+        echo json_encode($response);
+        Yii::app()->end();
+	}
+
+    public function update()
+    {
+        $response = array('success'=>'ok');
+        $commodity_d = Yii::app()->request->getParam('commodityId');
+        $price = Yii::app()->request->getParam('price');
+        $quantity = Yii::app()->request->getParam('quantity');
+        $vendor_id = Yii::app()->request->getParam('vendorId');
+        $commodity = VendorComodity::model()->findByAttributes(array('vendor_id'=>$vendor_id, 'comodity_id'=>$commodity_d));
+        if(isset($commodity)){
+            $commodity->price = $price;
+            $commodity->comodity_id = $commodity_d;
+            $commodity->quantity = $quantity;
+            $commodity->vendor_id = $vendor_id;
+            if(!$commodity->save()){
+                $response['success'] = 'failed';
+                $response['detail'] = $commodity->getErrors();
+            }
+        }else{
+            $response['success']='failed';
+            $response['detail']='not found';
+        }
+        header('Content-type: application/json');
+        echo json_encode($response);
+        Yii::app()->end();
+    }
+}
--- protected/modules/api/views/default/index.php
+++ protected/modules/api/views/default/index.php
@@ -9,5 +9,13 @@ $this->breadcrumbs=array(
 
 <p>
     <h3>Available</h3>
-    <ul><?php echo CHtml::link('markets', array('/api/market'))?></ul>
+    <ul>
+        <li><?php echo CHtml::link('markets', array('/api/market'))?></li>
+        <li>
+            <ul><?php echo CHtml::link('commodity', array('#'))?>
+                <li>Add: <?php echo Yii::app()->createAbsoluteUrl('/api/commodity/add', array('commodityId'=>'0000', 'price'=>'0000', 'quantity'=>'0000','vendorId'=>'0000'));?></li>
+                <li>Update: <?php echo Yii::app()->createAbsoluteUrl('/api/commodity/update', array('commodityId'=>'0000', 'price'=>'0000', 'quantity'=>'0000','vendorId'=>'0000'));?></li>
+            </ul>
+        </li>
+    </ul>
 </p>
